d582c94e7ea6af5ccc30dadacb7d0db35f2ca948
NerdSanctumDev <Joseluis_180302@hotmail.com>
2026-08-24 23:49:56 -0500

    Solucionando error

--- scripts/procesamiento.py
+++ scripts/procesamiento.py
@@ -1,9 +1,3 @@
-import os
-import sys
-
-# Forzar codificación UTF-8 en el entorno de ejecucion para Windows
-os.environ["PGCLIENTENCODING"] = "utf-8"
-
 import pandas as pd
 import psycopg2
 
@@ -13,70 +7,67 @@ import psycopg2
 df = pd.read_csv("data/dataset.csv")
 print("Dataset original")
 print(df)
-
 # =========================
 # Limpieza de datos
 # =========================
+# Eliminación de duplicados
 df = df.drop_duplicates()
+# Reemplazo de valores nulos
 df = df.fillna(0)
 print("Dataset limpio")
 print(df)
-
 # =========================
 # Exportación de dataset limpio
 # =========================
 df.to_csv("output/dataset_limpio.csv", index=False)
 print("Archivo exportado correctamente")
-
 # =========================
 # Conexión PostgreSQL
 # =========================
-# =========================
-# Conexion PostgreSQL
-# =========================
-dsn = "postgresql://admin:admin123@127.0.0.1:5432/laboratorio"
-conn = psycopg2.connect(dsn)
+conn = psycopg2.connect(
+    host="localhost", database="laboratorio", user="admin", password="admin123"
+)
 cursor = conn.cursor()
 
-print("Conexion PostgreSQL exitosa")
+print("Conexión PostgreSQL exitosa")
+
 # =========================
-# Creacion de tabla
+# Creación de tabla
 # =========================
+
 cursor.execute(""" 
+ 
 CREATE TABLE IF NOT EXISTS clientes ( 
     id INT, 
     nombre VARCHAR(50), 
     edad INT, 
     ciudad VARCHAR(50) 
 ) 
+ 
 """)
 
 conn.commit()
+
 print("Tabla creada correctamente")
 
 # =========================
 # Inserción de registros
 # =========================
+
 cursor.execute("DELETE FROM clientes")
 conn.commit()
 
 for index, row in df.iterrows():
+
     cursor.execute(
         """ 
         INSERT INTO clientes (id, nombre, edad, ciudad) 
         VALUES (%s, %s, %s, %s) 
         """,
-        (
-            int(row["id"]),
-            str(row["nombre"]),
-            int(float(row["edad"])),
-            str(row["ciudad"]),
-        ),
+        (int(row["id"]), row["nombre"], int(float(row["edad"])), row["ciudad"]),
     )
-
 conn.commit()
 print("Datos insertados correctamente")
-
 # =========================
 # Validación final
 # =========================
@@ -84,10 +75,8 @@ cursor.execute("SELECT * FROM clientes")
 resultado = cursor.fetchall()
 print(f"Total registros: {len(resultado)}")
 print("Datos almacenados en PostgreSQL")
-
 for fila in resultado:
     print(fila)
-
 # =========================
 # Cierre de conexión
 # =========================
